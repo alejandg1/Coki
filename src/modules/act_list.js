b53e74aa905784712675998f2713e13e3a48d0e7
alejandg1 <71184889+alejandg1@users.noreply.github.com>
2023-10-12 20:26:50 -0500

    solucionando problemas 1

--- src/modules/act_list.js
+++ src/modules/act_list.js
@@ -8,7 +8,7 @@ const datos_unidad_nombre = funciones.data(paths_array.datos_unidad_nombre);
 const filtro_tipo = document.querySelector("#filtro");
 
 function agregar_listeners() {
-  const btn_edit = document.querySelectorAll(".boton-lapiz");
+  const btn_edit = document.querySelectorAll(".boton_lapiz");
   btn_edit.forEach((boton) => {
     boton.addEventListener("click", (event) => {
       event.preventDefault();
@@ -16,7 +16,7 @@ function agregar_listeners() {
       ipcRenderer.send("editar", actividad);
     });
   });
-  const btn_desc = document.querySelectorAll(".svgButtonDesc ");
+  const btn_desc = document.querySelectorAll(".boton_descripcion");
   btn_desc.forEach((boton) => {
     boton.addEventListener("click", (event) => {
       event.preventDefault();
@@ -42,7 +42,7 @@ function agregar_listeners() {
       //NOTE: mostrar Descripción
     });
   });
-  const btn_push_act = document.querySelectorAll(".boton-agregar");
+  const btn_push_act = document.querySelectorAll(".boton_agregar");
   btn_push_act.forEach((boton) => {
     boton.addEventListener("click", (event) => {
       event.preventDefault();
@@ -66,14 +66,6 @@ function agregar_listeners() {
     });
   });
 }
-let svgButton = `<button class='boton-lapiz'>
-<svg class="edit-svgIcon" viewBox="0 0 512 512">
-<path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
-</svg></button>`;
-
-let svgButtonAdd = `<button class='boton-agregar' ><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12 5v14m-7-7h14'/></svg></button>`;
-
-let svgButtonDesc = `<button class='boton-descripcion'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M3 12h18M3 6h18M3 18h18'/></svg></button>`;
 // mostrar actividades segun el tipo de actividad escogido
 filtro_tipo.addEventListener("change", () => {
   while (Table.rows.length > 1) {
@@ -85,6 +77,14 @@ filtro_tipo.addEventListener("change", () => {
       ) &&
         (actividad.tipo == filtro_tipo.value || filtro_tipo.value == "todos")
       ) {
+let svgButton = `<button id="${funciones.formato_string(actividad.nombre,"reverse")}" class='boton_lapiz'>
+<svg class="edit-svgIcon" viewBox="0 0 512 512">
+<path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
+</svg></button>`;
+
+let svgButtonAdd = `<button id ="${funciones.formato_string(actividad.nombre,"reverse")}" class='boton_agregar' ><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12 5v14m-7-7h14'/></svg></button>`;
+
+let svgButtonDesc = `<button id="${funciones.formato_string(actividad.nombre,"reverse")}" class='boton_descripcion'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M3 12h18M3 6h18M3 18h18'/></svg></button>`;
         let linea =
           "<tr> <td>" +
           actividad.duracion +
@@ -101,7 +101,7 @@ filtro_tipo.addEventListener("change", () => {
       }
     });
   } else {
-    Table.insertAdjacentHTML("beforeend", "No existen actividades guardadas");
+    Table.insertAdjacentHTML("be<foreend", "No existen actividades guardadas");
   }
   agregar_listeners()
 })
--- src/modules/crono.js
+++ src/modules/crono.js
@@ -6,16 +6,16 @@ const Table = document.querySelector("Table");
 const Excel = document.querySelector("#xlsx");
 const btn_new_act = document.querySelector("#new_act");
 
-let svgButtonRemove = `<button class='boton-quitar'><svg viewBox="0 0 15 17.5" height="17.5" width="15" xmlns="http://www.w3.org/2000/svg" class="icon">
-<path transform="translate(-2.5 -1.25)" d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z" id="Fill"></path>
-</svg></button>`;
+
 
 if (
   actividades_json != undefined &&
   actividades_json != "[]" &&
   actividades_json.length > 0
 ) {
-  actividades_json.forEach((actividad) => {
+  actividades_json.forEach((actividad) => {let svgButtonRemove = `<button class='boton-quitar'><svg viewBox="0 0 15 17.5" height="17.5" width="15" xmlns="http://www.w3.org/2000/svg" class="icon">
+<path transform="translate(-2.5 -1.25)" d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z" id="Fill"></path>
+</svg></button>`;
     let linea =
       "<tr id='actividad' > <td>" +
       actividad.duracion + " min" +
