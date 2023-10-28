3a51b3995e9304e694a6d2876c4c6989f85f4110
Alejandro <josealejandrogomezparedes@gmail.com>
2023-10-27 19:48:57 -0500

    configurando makers

--- forge.config.js
+++ forge.config.js
@@ -5,37 +5,21 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: "@electron-forge/maker-zip",
+      name: '@electron-forge/maker-squirrel',
+      config: {},
     },
     {
-      name: "@electron-forge/maker-deb",
-      config: {
-        options: {
-          icon: "./images/logo.png"
-        }
-      },
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
     },
     {
-      name: "@electron-forge/maker-squirrel",
-      config: {
-        setupIcon: "./images/logo.ico",
-        certificateFile: "./cert.pfx",
-        certificatePassword: process.env.CERTIFICATE_PASSWORD,
-      },
-    },
-    {
-      name: '@electron-forge/maker-dmg',
-      config: {
-        name: "ActioMeta",
-        icon: "./images/logo.icns",
-        iconSize: 20,
-        format: 'ULFO'
-      }
+      name: '@electron-forge/maker-deb',
+      config: {},
     },
   ],
   plugins: [
     {
-      name: "@electron-forge/plugin-auto-unpack-natives",
+      name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
   ],
