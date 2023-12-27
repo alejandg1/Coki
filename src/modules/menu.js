bd281a97ec4c79b194cb7ccc344a538c5d8ae044
adre <josealejandrogomezparedes@gmail.com>
2023-12-27 18:55:54 -0500

    del: devtools

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -7,20 +7,20 @@ function setMenu() {
     },
   ];
   //TODO: quitar DevTools al pasar a prod
-  if (process.env.NODE_ENV !== "production") {
-    template.push({
-      label: "DevTools",
-      submenu: [
-        {
-          label: "Show/Hide",
-          click(item, focWin) {
-            focWin.toggleDevTools();
-          },
-        },
-        { role: "reload" },
-      ],
-    });
- }
+//   if (process.env.NODE_ENV !== "production") {
+//     template.push({
+//       label: "DevTools",
+//       submenu: [
+//         {
+//           label: "Show/Hide",
+//           click(item, focWin) {
+//             focWin.toggleDevTools();
+//           },
+//         },
+//         { role: "reload" },
+//       ],
+//     });
+//  }
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 }
